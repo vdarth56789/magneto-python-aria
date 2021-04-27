b4a1027e86c8add4780dc0a6df6eae06caea5c63
vdarth567 <67248106+vdarth567@users.noreply.github.com>
2021-04-27 20:17:44 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -10,10 +10,11 @@ if [ -f $file ] ; then
 fi
 tracker="[$tracker_list]"
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=10
+export MAX_CONCURRENT_DOWNLOADS=7
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
-   --bt-tracker=$tracker --bt-max-peers=0 --user-agent=qbittorrent/r/n
+   --bt-tracker=$tracker --bt-max-peers=0 \
+   --peer-id-prefix=-TR3000- --user-agent=Transmission/3.00
