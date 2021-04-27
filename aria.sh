6665ce9ce4bce69230788119206bfdb461877cb2
vdarth567 <67248106+vdarth567@users.noreply.github.com>
2021-04-27 19:28:34 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -3,5 +3,6 @@ export MAX_CONCURRENT_DOWNLOADS=7
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+  --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
+  --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+  --peer-id-prefix=-qB4250- --user-agent==qBittorrent/4.2.5
